c7d9063bb699f09227fb6e881eaa71eca5ca48fe
zfisch <zacharyrobert@gmail.com>
2015-03-25 16:29:13 -0700

    implement functional-shared pattern for stack and queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,33 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.count = 0;
+  obj.storage = {};
+  _.extend(obj, queueMethods);
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(val){
+    for(var i=0; i<this.count; i++){
+      this.storage[i+1] = this.storage[i];
+    }
+    this.count++;
+    this.storage[0] = val;
+  },
+  dequeue: function(){
+    var result;
+    if(this.count > 0){
+      this.count--;
+      result = this.storage[this.count];
+    }
+    return result;
+  },
+  size: function(){
+    return this.count;
+  }
+};
 
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var stack = {};
+  stack.count = 0;
+  stack.storage = {};
+
+  _.extend(stack, stackMethods);
+
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function() {
+    return this.count;
+  },
+
+  push: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  },
+
+  pop: function() {
+    var result;
+
+    if (this.count > 0) {
+      this.count--;
+      result = this.storage[this.count];
+    }
+
+    return result;
+  }
+};
 
 
