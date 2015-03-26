01cca43d67e88a25fff7cccfe94e87df2017b7b7
zfisch <zacharyrobert@gmail.com>
2015-03-25 20:57:19 -0700

    implement prototypal queue and stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
+
 };
 
+Stack.prototype.push = function(val){
+  this.storage[this.count] = val;
+  this.count++;
+};
 
+Stack.prototype.pop = function(){
+  var result;
+
+  if(this.count > 0){
+    this.count--;
+    result = this.storage[this.count];
+  }
+  return result;
+};
+
+Stack.prototype.size = function(){
+  return this.count;
+};
