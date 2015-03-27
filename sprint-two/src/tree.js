fa077a1e6c8fbceb697180f35668cbfc5ed4a483
zfisch <zacharyrobert@gmail.com>
2015-03-27 11:50:16 -0700

    implement tree with _.some and a for loop with helperfunc

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,24 +3,48 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
-
+  if (this.value === target){
+    return true;
+  } else {
+    return _.some(this.children, function(tree){
+      return tree.contains(target);
+    });
+  }
 };
+
+  // var result = false;
+  // if (this.value === target){
+  //   return true;
+  // }
+  // var helperFunc = function(arr){
+  //   _.each(arr, function(item){
+  //     if(item.value === target){
+  //       result = true;
+  //     }
+  //     helperFunc(item.children);
+  //   });
+  // };
+  // helperFunc(this.children);
+  // return result;
+
+
+  // return this.value === target || _.some(this.children, function(node){
+  //     return node.contains(target);
+  //   });
 
 
 /*
